607df1e8b5bcce04b031eab2c124c26853d6b6dd
GMinetto_Dev <gminetto_dev@proton.me>
2025-11-30 15:44:19 -0300

    Error handler and log configuration with Winston

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
-const app = require('./src/app');
+const { app, logger } = require('./src/app');
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
-  console.log(`Server running on: http://localhost:${PORT}`);
+  logger.info(`Server running on: http://localhost:${PORT}`);
 });
--- backend/src/app.js
+++ backend/src/app.js
@@ -8,12 +8,15 @@ const app = express();
 const helmet = require('helmet');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swagger');
-
+const { requestLogger } = require('./middlewares/requestLogger');
+const { errorHandler } = require('./middlewares/errorHandler');
+const { logger } = require('./config/logger');
 
 // Middlewares (global)
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
+app.use(requestLogger);
 
 // Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
@@ -24,4 +27,7 @@ app.use('/users', userRoutes);
 app.use('/events', eventRoutes);
 app.use('/protected', dashboardRoutes);
 
-module.exports = app;
+// Error Middleware
+app.use(errorHandler);
+
+module.exports = { app, logger };
